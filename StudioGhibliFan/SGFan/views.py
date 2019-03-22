4a544b36070e83cde9fdd2471dfe93659602ce47
vina <sj9962nf@go.minneapolis.edu>
2019-03-22 18:03:46 -0500

    Adding more comments

--- StudioGhibliFan/SGFan/views.py
+++ StudioGhibliFan/SGFan/views.py
@@ -7,7 +7,7 @@ from .forms import SGMoviesForm
 from django.views.generic import RedirectView
 
 # View inside the website
-#def home request, and return a respond with home.html
+# define to create movie
 def movie_create(request):
 	form = SGMoviesForm(request.POST or None)
 
@@ -21,21 +21,27 @@ def movie_create(request):
 	context = {
 		"form" : form
 	}
+	# returning to render request, movie form to be filled out page, and context of form.
 	return render(request, "SGFan/movie_form.html", context)
 
-
+# movie detail that return request and movie id.
 def movie_detail(request, id=None):
+	# instance equals to getting the object or show error 404 page from SGMovies by id.
 	instance = get_object_or_404(SGMovies, id=id)
 	context = {
 		"title" : instance.title,
 		"instance" : instance
 	}
+	# returning to render request, show the movie detail page, and context of title and instance.
 	return render(request, "SGFan/movie_detail.html", context)
 	 
-
+# movie list
 def movie_list(request):
+
+	# queryset equals to get all the objects in SGMovies order by number id.
 	queryset = SGMovies.objects.all().order_by("id")
 
+	# search bar, if the title contains the query that the user type show the title.
 	query = request.GET.get("q")
 	if query:
 		queryset = queryset.filter(title__icontains=query)
@@ -44,11 +50,14 @@ def movie_list(request):
 		"object_list":queryset,
 		"title" : "List"
 	}
+	# returning to render request, show the main page, and context of object_list and title.
 	return render(request, "SGFan/main.html", context)
 
-
+# update or edit the movie
 def movie_update(request, id=None):
+
 	instance = get_object_or_404(SGMovies, id=id)
+	
 	form = SGMoviesForm(request.POST or None, instance=instance)
 
 	# if all the form filled out with how it supposed to be then it will 
@@ -63,4 +72,5 @@ def movie_update(request, id=None):
 		"instance" : instance,
 		"form" : form
 	}
+	# returning to render request, show the movie detail page, and context of title, instance, form.
 	return render(request, "SGFan/movie_detail.html", context)

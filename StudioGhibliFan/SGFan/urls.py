7ce7638812c4b38bd63c04bcb807b76f88281080
vina <sj9962nf@go.minneapolis.edu>
2019-03-22 18:05:52 -0500

    Adding more comments

--- StudioGhibliFan/SGFan/urls.py
+++ StudioGhibliFan/SGFan/urls.py
@@ -8,8 +8,15 @@ from .views import (
 	)
 
 urlpatterns = [
+	# movie list path.
     url(r'^$', movie_list, name='list'),
+
+    # creating movie path.
     url(r'^create/$', movie_create, name='create'),
+
+    # movie detail path.
     url(r'^(?P<id>\d+)/$', movie_detail, name='detail'),
+    
+    # editing movie path.
    	url(r'^(?P<id>\d+)/edit/$', movie_update, name='update'),
 ]
